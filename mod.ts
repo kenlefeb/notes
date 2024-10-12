d881263e0cfffc73e78eb1c0aec155cd094c5bae
Ken LeFebvre <kenneth@lefebvre.us>
2024-10-12 17:57:55 -0400

    WIP: Aborting my current configuration approach to simplify

--- mod.ts
+++ mod.ts
@@ -8,7 +8,8 @@ const cli = sade("notes");
 
 cli
   .version("1.0.0")
-  .option("-c, --config", "Provide path to custom config", configFile);
+  .option("-c, --config", "Provide path to custom config", configFile)
+  .option("-d, --debug", "Enable debug mode", false);
 
 cli
   .command("daily add [date]")
@@ -18,16 +19,36 @@ cli
   .example("daily add 2021-01-01")
   .example("daily add")
   .action(async (date, options) => {
+    if (options.debug) {
+      console.log("Date:", date);
+      console.log("Options:", options);
+      console.log("Config File:", options.config ?? configFile);
+    }
+
     const configuration = await Configuration.load(
       options.config ?? configFile
     );
+
+    if (options.debug) {
+      console.log("Configuration:", configuration);
+      console.log("Vault Path:", configuration.paths.vault);
+    }
+
     const theDate: Date = date ? dayjs(date).toDate() : new Date();
+
+    if (options.debug) {
+      console.log("Date:", dayjs(theDate).format("YYYY-MM-DD"));
+    }
+
     const note: DailyNote = await DailyNote.create(theDate, configuration);
 
     console.log(
       `Created new Daily Note for ${dayjs(note.date).format("YYYY-MM-DD")}`
     );
-    console.log(`> ${note.path}`);
+
+    if (options.debug) {
+      console.log("Note Path:", note.path);
+    }
   });
 
 cli.parse(process.argv);
--- src/configuration.ts
+++ src/configuration.ts
@@ -2,69 +2,93 @@ import path from "node:path";
 import { FileSystem } from "./FileSystem.ts";
 import _ from "npm:lodash";
 import { isAbsolute } from "node:path/posix";
+import { z } from "npm:zod";
+import { load } from "https://deno.land/x/config/mod.ts";
 
 export class Configuration {
-  today: Date = new Date();
+  constructor() {
+    const schema = z.object({
+      paths: z.object({
+        vault: z
+          .string()
+          .default(path.resolve(FileSystem.homeDirectory, "notes")),
+        journal: z.string().default("@"),
+        rolodex: z.string().default("="),
+        encyclopedia: z.string().default("#"),
+        library: z.string().default("$"),
+      }),
+    });
+  }
+
   paths: Paths = new Paths();
 
   public static get defaultFile(): string {
     return path.join(FileSystem.homeDirectory, ".notes.json");
   }
 
-  public static async load(path: string) {
-    if (FileSystem.fileExists(path)) {
-      return await loadFromFile();
+  public static async load(filespec: string): Promise<Configuration> {
+    const config = await load({ file: filespec });
+    const parsed = schema.parse(config);
+
+    if (FileSystem.fileExists(filespec)) {
+      const json = await Deno.readTextFile(filespec);
+      return JSON.parse(json);
     } else {
       return new Configuration();
-    }
-
-    async function loadFromFile() {
-      const configData = JSON.parse(await Deno.readTextFile(path));
-      const config = new Configuration();
-      return _.merge(config, configData);
     }
   }
 }
 
 export class Paths {
-  private _vault: string = FileSystem.resolve("~/notes");
+  // VAULT - the root of it all
+  private _vault: string = "";
   public get vault(): string {
     return this._vault;
   }
-  public set value(vault: string) {
-    this._vault = path.resolve(vault);
+  public set vault(value: string) {
+    this._vault = value ?? path.resolve(FileSystem.homeDirectory, "notes");
   }
 
-  private _journal: string = path.resolve(this.vault, "@"); // This contains daily notes
+  // JOURNAL - for daily notes
+  private _journal: string = this.resolve("@");
   public get journal(): string {
     return this._journal;
   }
   public set journal(value: string) {
     this._journal = this.resolve(value);
   }
-  private _rolodex: string = path.resolve(this.vault, "="); // This contains notes about specific entities (people, places, things)
+
+  // ROLODEX - for entities
+  private _rolodex: string = this.resolve("=");
   public get rolodex(): string {
     return this._rolodex;
   }
   public set rolodex(value: string) {
     this._rolodex = this.resolve(value);
   }
-  private _encyclopedia: string = path.resolve(this.vault, "#"); // This contains notes about topics
+
+  // ENCYCLOPEDIA - for reference material
+  private _encyclopedia: string = this.resolve("#");
   public get encyclopedia(): string {
-    return this._encyclopedia;
+    return this.resolve(this._encyclopedia ?? "#");
   }
   public set encyclopedia(value: string) {
     this._encyclopedia = this.resolve(value);
   }
-  private _library: string = path.resolve(this.vault, "$"); // This contains system files, such as templates
+
+  // LIBRARY - for scripts and other utilities
+  private _library: string = this.resolve("$");
   public get library(): string {
-    return this._library;
+    return this.resolve(this._library ?? "$");
   }
   public set library(value: string) {
     this._library = this.resolve(value);
   }
 
   private resolve(value: string): string {
-    return isAbsolute(value) ? value : path.resolve(this.vault, value);
+    if (isAbsolute(value)) {
+      return value;
+    }
+    return path.resolve(this.vault, value);
   }
 }
